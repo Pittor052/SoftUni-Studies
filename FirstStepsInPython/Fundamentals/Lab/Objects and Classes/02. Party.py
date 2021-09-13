3ea7aae1701d155b002eff55671b2a05bde0e659
Pittor052 <lazar_off@yahoo.com>
2021-09-13 21:41:30 +0300

    Adding lecture objects and classes

---
+++ FirstStepsInPython/Fundamentals/Lab/Objects and Classes/02. Party.py
@@ -0,0 +1,13 @@
+class Party:
+    def __init__(self):
+        self.people = []
+
+
+party = Party()
+line_input = input()
+while not line_input == "End":
+    party.people.append(line_input)
+    line_input = input()
+print(f"Going: {', '.join(party.people)}")
+print(f"Total: {len(party.people)}")
+
